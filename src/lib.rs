588c722b892f6568716ad57b2822414799003860
Fenhl <fenhl@fenhl.net>
2021-02-07 19:28:21 +0000

    Crate-level doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! This is a [Rust](https://rust-lang.org/) library to help you create chat bots for [racetime.gg](https://racetime.gg/).
+//!
+//! For documentation, see also <https://github.com/racetimeGG/racetime-app/wiki/Category-bots>.
+
 #![deny(rust_2018_idioms, unused, unused_crate_dependencies, unused_import_braces, unused_qualifications, warnings)]
 #![forbid(unsafe_code)]
 
