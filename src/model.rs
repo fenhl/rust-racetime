71ac9f569342ea4371a36eda9390deeaeb325ed4
Fenhl <fenhl@fenhl.net>
2023-09-05 07:44:22 +0000

    Fix serde capitalization of SurveyQuestionKind

--- src/model.rs
+++ src/model.rs
@@ -309,6 +309,7 @@ pub struct SurveyQuestion {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
 pub enum SurveyQuestionKind {
     Input,
     Bool,
